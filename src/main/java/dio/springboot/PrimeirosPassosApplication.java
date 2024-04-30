c434b7ace1ce9e8a28d5a030854c6f53f6b4e6b0
Ives Chiba <ivesjundi@gmail.com>
2024-04-30 12:05:45 -0300

    feat :sparkles: class 3: beans vs components

--- src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,13 +1,29 @@
 package dio.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrimeirosPassosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeirosPassosApplication.class, args);
+    }
+
+    // @Bean : quando não temos acesso ao código-fonte. Ex: Gson
+    // @Component : quando a classe foi provida por mim. Ex: ViaCepResponse
+
+
+    @Bean
+    public CommandLineRunner run(ConversorJson conversor) throws Exception {
+        return args -> {
+            String json = "{\"cep\": \"01001-000\", \"logradouro\": \"Praça da Sé\", \"localidade\": " +
+                    "\"São Paulo\"}";
+            ViaCepResponse response = conversor.converter(json);
+            System.out.println("Dados do CEP: " + response);
+        };
+    }
 
 }
